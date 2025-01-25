b7b24ddc2af56106c2550faa193ee5fc70a9b961
Romani Todua <romachkuna@gmail.com>
2025-01-25 17:59:11 +0400

    example for custom unmarshaller

--- out/example.go
+++ out/example.go
@@ -1,1 +1,22 @@
 package out
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
+type Example struct {
+}
+
+func (a Example) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+func (a *Example) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(b, &a)
+}
